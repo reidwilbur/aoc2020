f9eac4cb40941f1f0a4f2d01691bdfd2da5838cf
Reid Wilbur <reid.wilbur@gmail.com>
2020-12-02 10:11:53 -0600

    Updating deps and using immutables

--- src/main/java/com/wilb0t/aoc/Day2.java
+++ src/main/java/com/wilb0t/aoc/Day2.java
@@ -1,42 +1,57 @@
 package com.wilb0t.aoc;
 
+import org.immutables.value.Value;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 class Day2 {
 
-  public static List<String> getValidPasswordsRangeRule(List<String> input) {
+  @Value.Immutable
+  public interface PasswordEntry {
+    int lo();
+
+    int hi();
+
+    char letter();
+
+    String password();
+
+    static PasswordEntry fromString(String line) {
+      var parts = line.split(" ");
+      var rangeParts = parts[0].split("-");
+
+      var lo = Integer.parseInt(rangeParts[0]);
+      var hi = Integer.parseInt(rangeParts[1]);
+      var letter = parts[1].charAt(0);
+
+      return ImmutablePasswordEntry.builder()
+          .lo(lo)
+          .hi(hi)
+          .letter(letter)
+          .password(parts[2])
+          .build();
+    }
+  }
+
+  public static List<PasswordEntry> getValidPasswordsRangeRule(List<String> input) {
     return input.stream()
-        .filter(line -> {
-          var parts = line.split(" ");
-          var rangeParts = parts[0].split("-");
-          
-          var lo = Integer.parseInt(rangeParts[0]);
-          var hi = Integer.parseInt(rangeParts[1]);
-          var letter = parts[1].charAt(0);
-          
-          var letterCount = parts[2].chars().filter(i -> i == letter).count();
-          return (letterCount >= lo) && (letterCount <= hi);
-        })
+        .map(PasswordEntry::fromString)
+        .filter(
+            entry -> {
+              var letterCount =
+                  entry.password().chars().filter(i -> i == entry.letter()).count();
+              return (letterCount >= entry.lo()) && (letterCount <= entry.hi());
+            })
         .collect(Collectors.toList());
   }
-  
-  public static List<String> getValidPasswordsPositionRule(List<String> input) {
+
+  public static List<PasswordEntry> getValidPasswordsPositionRule(List<String> input) {
     return input.stream()
-        .filter(line -> {
-          var parts = line.split(" ");
-          var rangeParts = parts[0].split("-");
-
-          var pos1 = Integer.parseInt(rangeParts[0]);
-          var pos2 = Integer.parseInt(rangeParts[1]);
-          var letter = parts[1].charAt(0);
-
-          return (parts[2].charAt(pos1 - 1) == letter) ^ (parts[2].charAt(pos2 - 1) == letter);
-        })
+        .map(PasswordEntry::fromString)
+        .filter(
+            entry ->
+                (entry.password().charAt(entry.lo() - 1) == entry.letter())
+                    ^ (entry.password().charAt(entry.hi() - 1)) == entry.letter())
         .collect(Collectors.toList());
   }
 }
