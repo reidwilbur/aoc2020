7d1e72f9a9ec49ef78ba87dbbdb9340434e2ab70
Reid Wilbur <reid.wilbur@gmail.com>
2020-12-02 11:27:48 -0600

    Use java 15 records

--- src/main/java/com/wilb0t/aoc/Day2.java
+++ src/main/java/com/wilb0t/aoc/Day2.java
@@ -1,35 +1,20 @@
 package com.wilb0t.aoc;
 
-import org.immutables.value.Value;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Day2 {
 
-  @Value.Immutable
-  public interface PasswordEntry {
-    int lo();
-
-    int hi();
-
-    char letter();
-
-    String password();
-
-    static PasswordEntry fromString(String line) {
+  public static record PasswordEntry(int lo, int hi, char letter, String password) {
+    public static PasswordEntry fromString(String line) {
       var parts = line.split(" ");
       var rangeParts = parts[0].split("-");
 
       var lo = Integer.parseInt(rangeParts[0]);
       var hi = Integer.parseInt(rangeParts[1]);
       var letter = parts[1].charAt(0);
-
-      return ImmutablePasswordEntry.builder()
-          .lo(lo)
-          .hi(hi)
-          .letter(letter)
-          .password(parts[2])
-          .build();
+      
+      return new PasswordEntry(lo, hi, letter, parts[2]);
     }
   }
 
